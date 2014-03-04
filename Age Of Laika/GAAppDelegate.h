868ac1ffa8d1686bf0a50551d797939f76968ea9
Gabriel Aguilera <gabrieag@umich.edu>
2014-03-04 02:42:50 -0500

    Initial Commit

---
+++ Age Of Laika/GAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GAAppDelegate.h
+//  Age Of Laika
+//
+//  Created by Gabriel Aguilera on 3/4/14.
+//  Copyright (c) 2014 Gabriel Aguilera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Age Of LaikaTests/Age_Of_LaikaTests.m
@@ -0,0 +1,34 @@
+//
+//  Age_Of_LaikaTests.m
+//  Age Of LaikaTests
+//
+//  Created by Gabriel Aguilera on 3/4/14.
+//  Copyright (c) 2014 Gabriel Aguilera. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Age_Of_LaikaTests : XCTestCase
+
+@end
+
+@implementation Age_Of_LaikaTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
